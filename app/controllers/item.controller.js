81d67a924c6ba34cf81d75c8e0f9d43f5e30d8f6
thiagoaraujo0007 <lordplay0007@gmail.com>
2023-08-26 11:14:20 -0300

    alteracao controle

--- app/controllers/item.controller.js
+++ app/controllers/item.controller.js
@@ -1,13 +1,45 @@
 const db = require("../models");
-const Items = db.item;
+const Item = db.item;
 const Op = db.Sequelize.Op;
 
 exports.create = (req,res) => {
-
-};
-
+   if (!req.body.name) {
+       res.status(400).send({
+           message: "o conteudo não pode ser vazio!"
+       });
+       return;
+   }
+   const item = {
+       name: req.body.name,
+       decription: req.body.description,
+       quantity: req.body.quantity,
+       is_flammable: req.body.is_flammable ? req.body.is_flammable : false
+        }
+       Item.create(item)
+       .them(data =>{
+           res.send(data);
+       })
+     .catch(err =>{
+         res.status(500).send({
+             message:
+             err.message || "Ocorreu um erro ao criar o item."
+         })
+     }) 
+    };
 exports.findALL = (req,res) => {
+   const name = req.body.name;
+   var condition = name ? { name: { [Op.like]: `%${name}%` }} : null;
 
+   Item.findALL({where: condition})
+   .them(data =>{
+       res.send(data);
+   })
+ .catch(err =>{
+     res.status(500).send({
+         message:
+         err.message || "Ocorreu um erro ao criar ao listar os item."
+     })
+ }) 
 };
 
 exports.findOne = (req,res) => {
